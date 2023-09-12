c2a16ba21d22508d46086554b36e284b399dcf0e
gloriamok <gloriamok15@gmail.com>
2023-09-12 06:02:02 +0000

    fix: Edit Solution of Week4/Day5 Problem

--- Week4/Day5/Solution.cpp
+++ Week4/Day5/Solution.cpp
@@ -1,54 +1,77 @@
 // 문제 20. 연결 요소 제거하기
-#include <bits/stdc++.h>
-#define pii pair<int, int>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
 using namespace std;
 
-const int MAX = 55;
-char C[MAX][MAX];
-int visited[MAX][MAX];
+int N, K, Q, y, x, di[4] = {1,-1,0,0}, dj[4] = {0,0,1,-1};
+char arr[51][51]{}, d;
+unordered_map<char,int> m;
 
-void BFS(int N, int y, int x, int lim) {
-    memset(visited, 0, sizeof(visited));
-    vector<pii> same;
-    queue<pii> Q;
-    Q.emplace(y, x);
-    visited[y][x] = 1;
-    char c = C[y][x];
-    while (!Q.empty()) {
-        auto [cy, cx] = Q.front(); Q.pop();
-        same.emplace_back(cy, cx);
-        for (int i = 0; i < 4; ++i) {
-            int ny = cy + "2101"[i] - '1', nx = cx + "1012"[i] - '1';
-            if (min(ny, nx) < 1 || max(ny, nx) > N || visited[ny][nx] || C[ny][nx] != c) continue;
-            visited[ny][nx] = 1;
-            Q.emplace(ny, nx);
-        }
-    }
-    
-    if (same.size() >= lim) {
-        for (auto [cy, cx] : same) {
-            C[cy][cx] = '.';
-        }
-    }
+bool isGreater(int i, int j, bool visited[][51]) {
+	visited[i][j] = 1;
+	int num = 1;
+	queue<pair<int,int>> q;
+	q.push({i,j});
+	while(!q.empty()) {
+		int i = q.front().first, j = q.front().second;
+		q.pop();
+		for(int k=0;k<4;k++) {
+			int ni = i + di[k], nj = j + dj[k];
+			if (ni > 0 && ni <= N && nj > 0 && nj <= N && !visited[ni][nj] && arr[ni][nj] == arr[i][j]) {
+				visited[ni][nj] = 1;
+				num++;
+				if (num >= K) return true;
+				q.push({ni,nj});
+			}
+		}
+	}
+	return false;
+}
+
+void deleteLetter(int i, int j) {
+	bool visited[51][51]{};
+	visited[i][j] = 1;
+	int alphabet = arr[i][j];
+	arr[i][j] = '.';
+	m[arr[i][j]]--;
+	queue<pair<int,int>> q;
+	q.push({i,j});
+	while(!q.empty()) {
+		int i = q.front().first, j = q.front().second;
+		q.pop();
+		for(int k=0;k<4;k++) {
+			int ni = i + di[k], nj = j + dj[k];
+			if (ni > 0 && ni <= N && nj > 0 && nj <= N && !visited[ni][nj] && arr[ni][nj] == alphabet) {
+				visited[ni][nj] = 1;
+				arr[ni][nj] = '.';
+				m[arr[ni][nj]]--;
+				q.push({ni,nj});
+			}
+		}
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int N, K, Q;
-    cin >> N >> K >> Q;
-    for (int i = 1; i <= N; ++i) for (int j = 1; j <= N; ++j) cin >> C[i][j];
-
-    while (Q--) {
-        int y, x;
-        char c;
-        cin >> y >> x >> c;
-        C[y][x] = c;
-        BFS(N, y, x, K);
-    }
-
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) cout << C[i][j];
-        cout << '\n';
-    }
-    return 0;
+	cin >> N >> K >> Q;
+	for(int i=1;i<=N;i++) for(int j=1;j<=N;j++) {
+		cin >> arr[i][j];
+		if (arr[i][j] != '.') m[arr[i][j]]++;
+	}
+	for(int num=1;num<=Q;num++) {
+		cin >> y >> x >> d;
+		arr[y][x] = d;
+		m[arr[y][x]]++;
+		bool visited[51][51]{};
+		for(int i=1;i<=N;i++) for(int j=1;j<=N;j++) {
+			if (!visited[i][j] && m[arr[i][j]] >= K) {
+				if (isGreater(i,j,visited)) deleteLetter(i,j);
+			}
+		}
+	}
+	for(int i=1;i<=N;i++) {
+		for(int j=1;j<=N;j++) cout << arr[i][j];
+		cout << "\n";
+	}
 }
